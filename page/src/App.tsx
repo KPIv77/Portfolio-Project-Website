fc3dc5fbd97144d42049b2576ee14871b35b9f20
KPIv77 <kitisakpholor@gmail.com>
2026-05-21 16:32:09 +0700

    revise

--- page/src/App.tsx
+++ page/src/App.tsx
@@ -195,6 +195,7 @@ export default function App() {
 						<span className="badge">SQL</span>
 						<span className="badge">Vercel</span>
 						<span className="badge">Render</span>
+						<span className="badge">Express</span>
 					</div>
 				</div>
 
